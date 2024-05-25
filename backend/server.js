53f3bd2a879b8e8a8ea0d0555f16735f3fb8806c
Anukul <sommer.anukul99@gmail.com>
2024-05-25 11:56:15 +0200

    init

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
+// Port for server to listen. If theres a Port env it will use that
+// otherwise it will use 5000
+const PORT = process.env.PORT || 5000;
+
+// Get req to / and respond for controlling
 app.get("/", (req, res) => {
     res.send("server running");
 });
 
-app.listen(5000, () => console.log("server running on port 5000"));
+// Start server and listen on port 5000
+app.listen(PORT, () => console.log(`server running on port 5000`));
