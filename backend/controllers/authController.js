1987aafe3c56ef624437e5b5daf59d14cbb5867f
anukul <sommer.anukul99@gmail.com>
2024-06-08 01:42:39 +0200

    signup to db

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,19 +1,63 @@
-export const signup = async(req, res) => {
-    try{
-        const {
-            fullName,
-            username,
-            password,
-            confirmPassword,
-            gender
-        } = req.body;
-    } catch (error) {}
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    // check password
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "passwords dont match" });
+    }
+
+    // checks if user exists
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "username already exists" });
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // https://avatar-placeholder.iran.liara.run/document
+    const boyPfp = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlPfp = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    // creating user from the passed inputs
+    const newUser = new User({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyPfp : girlPfp,
+    });
+
+    // save user to db
+    if (newUser) {
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+        res.status(400).json({error:"invalid user data"});
+    }
+    
+  } catch (error) {
+    console.log("error in signup controller", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
 };
 
 export const login = (req, res) => {
-    console.log("loginUser");
-}
+  console.log("loginUser");
+};
 
 export const logout = (req, res) => {
-    console.log("logoutUser");
-}
+  console.log("logoutUser");
+};
--- backend/server.js
+++ backend/server.js
@@ -7,18 +7,22 @@ import connectMongo from './db/connectMongo.js';
 const app = express();
 
 // Port for server to listen
-dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+// parse incoming req with json payloads
+app.use(express.json());
+// use auth routes
+app.use("/api/auth", authRoutes);
 
 // root route
 app.get("/", (req, res) => {
-    res.send("server running");
+  res.send("server running");
 });
-
-app.use("/api/auth",authRoutes)
 
 // Start server and listen on port 5000
 app.listen(PORT, () => {
     connectMongo();
-    console.log(`server running on port ${PORT}`)
+    console.log(`server running on port ${PORT}`);
 });
